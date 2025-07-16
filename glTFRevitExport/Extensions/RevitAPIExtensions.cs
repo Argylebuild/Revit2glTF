132d20cbad55e64b641cc41f387505bd462599a3
Logan Smith <lmaxsmith@gmail.com>
2025-07-16 15:19:14 -0700

    include 2025 and 2026 targets. Update some precompiles

--- glTFRevitExport/Extensions/RevitAPIExtensions.cs
+++ glTFRevitExport/Extensions/RevitAPIExtensions.cs
@@ -77,19 +77,10 @@ namespace GLTFRevitExport.Extensions {
 
         public static double ToGLTF(this Parameter p, double value) {
             // TODO: read value unit and convert correctly
-#if REVIT2020 
-            switch (p.Definition.UnitType) {
-                case UnitType.UT_Length:
-                    return value.ToGLTFLength();
-                default:
-                    return value;
-            }
-#else
 			string typeId = p.Definition.GetDataType().TypeId;
 			if (typeId.StartsWith("autodesk.spec.aec:length"))
 				return value.ToGLTFLength();
 			return value;
-#endif
 		}
 
         public static Tuple<string,object> ToGLTF(this Parameter param) {
